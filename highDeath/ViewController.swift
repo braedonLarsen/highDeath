516fe7393be463ebe95192e449473c161e211dc6
Braedon Larsen <braedon.larsen23@d155.org>
2022-11-30 08:04:37 -0600

    Initial Commit

---
+++ highDeath/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  highDeath
+//
+//  Created by BRAEDON LARSEN on 11/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
